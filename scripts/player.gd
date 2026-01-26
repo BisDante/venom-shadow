6914aab58f163e3d2fb802c151a6c355741963e2
Rafael Bispo Braúna <rafael@SmokingMonkey.smokingmonkey.com.br>
2026-01-26 14:37:12 -0300

    tiro viaja na direção certa e aparece nas posições certas

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 const bullet = preload("res://scenes/bullet.tscn")
+@onready var gun_pivot = $GunPivot
 @onready var gun_tip = $GunPivot/GunTip
 
 enum State {IDLE, RUN, CROUCH, JUMP, FALL, DEATH, SPAWNING, WIN}
@@ -10,7 +11,7 @@ enum AimState {UP, DOWN, FORWARD}
 
 var curr_state : State = State.IDLE
 var curr_aim_state : AimState = AimState.FORWARD
-var direction = 1
+var facing = 1 # 1 direita -1 esquerda
 
 func enter_state(state: State):
 	curr_state = state
@@ -39,19 +40,24 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	if Input.is_action_just_pressed("shoot"):
-		var angle = 0
-		if Input.is_action_pressed("ui_up"):
-			angle = -90
-		var new_bullet = bullet.instantiate()
-		new_bullet.set_angle(angle)
-		get_parent().add_child(new_bullet)
-		new_bullet.global_position = gun_tip.global_position
-		
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
+		if is_on_floor():
+			facing = sign(direction)
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	var shoot_angle = 0 + 90 * (facing - 1)
+	
+	if Input.is_action_just_pressed("shoot"):
+		if Input.is_action_pressed("ui_up"):
+			shoot_angle = -90
+			
+		gun_pivot.rotation_degrees = shoot_angle
+		var new_bullet = bullet.instantiate()
+		new_bullet.set_angle(shoot_angle)
+		get_parent().add_child(new_bullet)
+		new_bullet.global_position = gun_tip.global_position
 
 	move_and_slide()
