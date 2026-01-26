2fa1f731417403a8e4ba63f90a0c9faec5e293a7
Adler Targino <adler.t2001@gmail.com>
2026-01-26 13:38:15 -0300

    ajuste de referência

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
-const bullet = preload("res://scene/bullet.tscn")
+const bullet = preload("res://scenes/bullet.tscn")
 @onready var gun_tip = $GunPivot/GunTip
 
 enum State {IDLE, RUN, CROUCH, JUMP, FALL, DEATH, SPAWNING, WIN}
