b05cefdf27d7da9952f1304f25e323999f555203
Rafael Bispo Braúna <rbbrauna@gmail.com>
2026-01-30 16:02:43 -0300

    adicionado o modo amarelo que dá dano nos dois outros modos

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-enum Mode {GREEN, PURPLE}
+enum Mode {GREEN, PURPLE, YELLOW}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
